62062bdf3f6adaba4151eda1885213e82da49a25
Hyuntae-91 <tangyi7191@gmail.com>
2018-10-02 13:46:07 -0700

    V 1.04  Bug fix and print list function added

--- python/Data_Structure/Linked_List/Doubly_Linked_List.py
+++ python/Data_Structure/Linked_List/Doubly_Linked_List.py
@@ -6,25 +6,34 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None
 
-class Singly_Linked_List:
+class Doubly_Linked_List:
     def __init__(self):
-        self.head = Node(None) # Dummy Node
-        self.tail = self.head
+        self.head = Node(None) # dummy node
+        self.tail = Node(None) # dummy node
+        self.head.next = self.tail
         self.count = 0
     def list_add(self, data):
         newNode = Node(data) # Create New Node to add
-        self.tail.next = newNode # Set tail's next node to New Node
-        self.tail = newNode # Set tail point to New Node
+        pointer = self.head
+        while pointer.next is not self.tail:
+            pointer = pointer.next
+        pointer.next = newNode # Set tail's next node to New Node
+        newNode.prev = pointer # Set newNode's previous node to end data of the list
+        newNode.next = self.tail
+        self.tail.prev = newNode
         self.count += 1 # Increase size by 1
     def list_remove(self, data):
-        pointer = self.head# set pointer as head
+        pointer = self.head # set pointer as head
         if self.head.next == self.tail:
             print("List is Empty") # if list is empty, print this message, and return
             return
         while pointer.next is not self.tail: # Follow next pointer until end of list
             if pointer.next.data == data: # if next Node's data is same with data which want to delete
-                pointer.next = pointer.next.next # point current Node's next pointer to next next Node
+                temp = pointer.next
+                pointer.next = temp.next # point current Node's next pointer to next next Node
+                temp.next.prev = pointer # set temp's previous node to pointer
                 print("Remove successfully")
                 self.count -= 1
                 return 
