99e5142e3662c5ae51a4f6c817bd9721a30cfbf1
Hyuntae-91 <hyuntange91@gmail.com>
2018-10-04 16:22:54 -0700

    Added Stack data structure and test code

---
+++ python/Data_Structure/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from Stack import *
+
+st1 = Stack()
---
+++ python/test_alrorithms/Stack.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from Data_Structure.Stack import *
+
+st1 = Stack.Stack()
+for i in range(1, 11):
+    st1.push(i)
+print("Stack Size : ", st1.StackSize())
+print("pop : ", st1.pop())
+print("Stack Size : ", st1.StackSize())
+
+print("Is Empty? ", st1.is_empty())
+
+print("pop : ", st1.pop())
+print("pop : ", st1.pop())
+print("pop : ", st1.pop())
+
+print("Stack Size : ", st1.StackSize())
