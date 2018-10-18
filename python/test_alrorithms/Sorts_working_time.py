ad957ecc6beae3fb464b3e91a99a20565d0976a0
Hyuntae-91 <tangyi7191@gmail.com>
2018-10-18 14:21:19 -0700

    Refectoring Quick sort algorithm

---
+++ python/test_alrorithms/Sorts_working_time.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import random
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from Algorithms.Sorts import *
+
+li = []
+SIZE = 10000
+rand_num = random.randint(0, SIZE)
+
+for i in range(SIZE):              # create 10,000 random integer list for checking working time
+    while rand_num in li:
+        rand_num = random.randint(0, SIZE)
+
+    li.append(rand_num)
+
+
+
+start_time = time.time()
+print("")
+print("== Bubble Sorts ==")
+Bubble_sort.Bubble_sort(li)
+print("--- %.7s Seconds ---" % (time.time() - start_time))
+
+
+print("")
+print("")
+start_time = time.time()
+print("== Selection Sorts ==")
+Selection_sort.Selection_sort(li)
+print("--- %.7s Seconds ---" % (time.time() - start_time))
+
+
+print("")
+print("")
+start_time = time.time()
+print("== Insertion Sorts ==")
+Insertion_sort.Insertion_sort(li)
+print("--- %.7s Seconds ---" % (time.time() - start_time))
+
+
+print("")
+print("")
+start_time = time.time()
+print("== Quick Sorts ==")
+Quick_sort.Quick_sort(li)
+print("--- %.7s Seconds ---" % (time.time() - start_time))
+
+
+print("")
+print("")
+start_time = time.time()
+print("== Merge Sorts ==")
+Merge_sort.Merge_sort(li)
+print("--- %.7s Seconds ---" % (time.time() - start_time))
