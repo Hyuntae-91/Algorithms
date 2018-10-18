3ae4bf6307ecd3ac7ca455f7f1049ba1520ba09f
Hyuntae-91 <hyuntange91@gmail.com>
2018-10-18 14:21:19 -0700

    Refectoring Quick sort algorithm

--- python/Algorithms/Sorts/Quick_sort.py
+++ python/Algorithms/Sorts/Quick_sort.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def Quick_sort(_list, first, last):
-    if first < last:
-        p = Partition(_list, first, last)
-        Quick_sort(_list, first, p - 1)
-        Quick_sort(_list, p + 1, last)
-    return _list
+def Quick_sort(_list):
+    if len(_list) <= 1:
+        return _list
 
-def Partition(_list, first, last):
-    pivot = _list[int((first + last) / 2)]
-    left = first 
-    right = last
+    mid = Partition(_list)
+    left = _list[:mid]
+    right = _list[mid:]
+    left = Quick_sort(left)
+    right = Quick_sort(right)
+    return left + right
+
+def Partition(_list):
+    pivot = _list[int((len(_list)) / 2)]
+    left = 0 
+    right = len(_list) - 1
 
     while left < right:
         while (_list[left] < pivot) and (left < right):
