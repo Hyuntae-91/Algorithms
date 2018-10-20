06d3de60e5272a28c0061234a7e49b8c5e9ee28b
hyuntae-91 <hyuntange91@gmail.com>
2018-10-20 01:24:31 -0700

    Bug Fix of Quick sort and test sorting files

--- python/test_alrorithms/Sorts_working_time.py
+++ python/test_alrorithms/Sorts_working_time.py
@@ -10,51 +10,56 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from Algorithms.Sorts import *
 
 li = []
-SIZE = 10000                    # create SIZE of random integer list for checking working time
-rand_num = random.randint(0, SIZE)
+LIST_SIZE = 10000           
+NUM_SCOPE = 100000          
+rand_num = random.randint(0, NUM_SCOPE)
 
-for i in range(SIZE):              
+for i in range(LIST_SIZE):              
     while rand_num in li:
-        rand_num = random.randint(0, SIZE)
+        rand_num = random.randint(0, NUM_SCOPE)
 
     li.append(rand_num)
 
 
-
+copied_li = li[:]
 start_time = time.time()
 print("")
 print("== Bubble Sorts ==")
-Bubble_sort.Bubble_sort(li)
+Bubble_sort.Bubble_sort(copied_li)
 print("--- %.7s Seconds ---" % (time.time() - start_time))
 
 
+copied_li = li[:]
 print("")
 print("")
 start_time = time.time()
 print("== Selection Sorts ==")
-Selection_sort.Selection_sort(li)
+Selection_sort.Selection_sort(copied_li)
 print("--- %.7s Seconds ---" % (time.time() - start_time))
 
 
+copied_li = li[:]
 print("")
 print("")
 start_time = time.time()
 print("== Insertion Sorts ==")
-Insertion_sort.Insertion_sort(li)
+Insertion_sort.Insertion_sort(copied_li)
 print("--- %.7s Seconds ---" % (time.time() - start_time))
 
 
+copied_li = li[:]
 print("")
 print("")
 start_time = time.time()
 print("== Quick Sorts ==")
-Quick_sort.Quick_sort(li)
+Quick_sort.Quick_sort(copied_li)
 print("--- %.7s Seconds ---" % (time.time() - start_time))
 
 
+copied_li = li[:]
 print("")
 print("")
 start_time = time.time()
 print("== Merge Sorts ==")
-Merge_sort.Merge_sort(li)
+Merge_sort.Merge_sort(copied_li)
 print("--- %.7s Seconds ---" % (time.time() - start_time))
