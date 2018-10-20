4b58c6dc02d25ececd3d1fa9a60976cfbe30c123
Hyuntae-91 <tangyi7191@gmail.com>
2018-10-20 01:24:31 -0700

    Bug Fix of Quick sort and test sorting files

--- python/test_alrorithms/Sorts.py
+++ python/test_alrorithms/Sorts.py
@@ -8,29 +8,42 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from Algorithms.Sorts import *
 
 li = [2, 0, 10, 22, 11, 23, 5, 9, 14, 19]
+
+copied_li = li[:]
 print("")
 print("== Bubble Sorts ==")
-print(Bubble_sort.Bubble_sort(li))
+print(Bubble_sort.Bubble_sort(copied_li))
 print("==================")
-print("")
-print("")
 
 
+
+copied_li = li[:]
+print("")
+print("")
 print("== Selection Sorts ==")
-print(Selection_sort.Selection_sort(li))
+print(Selection_sort.Selection_sort(copied_li))
 print("==================")
+
+
+copied_li = li[:]
 print("")
 print("")
 print("== Insertion Sorts ==")
-print(Insertion_sort.Insertion_sort(li))
+print(Insertion_sort.Insertion_sort(copied_li))
 print("==================")
+
+
+copied_li = li[:]
 print("")
 print("")
 print("== Quick Sorts ==")
-print(Quick_sort.Quick_sort(li))
+print(Quick_sort.Quick_sort(copied_li))
 print("==================")
+
+
+copied_li = li[:]
 print("")
 print("")
 print("== Merge Sorts ==")
-print(Merge_sort.Merge_sort(li))
+print(Merge_sort.Merge_sort(copied_li))
 print("==================")
