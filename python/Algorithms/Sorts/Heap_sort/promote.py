4e9c57ce5e41dba20823fb1394fbb18e07b0daa4
hyuntae-91 <hyuntange91@gmail.com>
2018-10-30 23:59:41 -0700

    Added Acceleated Heap sort, and Test cases

--- python/Algorithms/Sorts/Heap_sort/promote.py
+++ python/Algorithms/Sorts/Heap_sort/promote.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def promote(_list, hStop, vacant, h):
+def promote(_list, hStop, vacant, h, size):
     if h <= hStop:
         vacStop = vacant
-    elif _list[2 * vacant + 1] <= _list[2 * vacant + 2]:
-        _list[vacant] = _list[2 * vacant + 2]
-        vacStop = promote(_list, hStop, 2 * vacant + 2, h - 1)
+    elif vacant * 2 + 1 >= size:
+        vacStop = -1
     else:
-        _list[vacant] = _list[2 * vacant + 1]
-        vacStop = promote(_list, hStop, 2 * vacant + 1, h - 1)
+        if _list[2 * vacant + 1] <= _list[2 * vacant + 2]:
+            _list[vacant] = _list[2 * vacant + 2]
+            vacStop = promote(_list, hStop, 2 * vacant + 2, h - 1, size)
+        else:
+            _list[vacant] = _list[2 * vacant + 1]
+            vacStop = promote(_list, hStop, 2 * vacant + 1, h - 1, size)
     return vacStop
 
